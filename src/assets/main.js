fe0a8d36c3f276fc3ed3e5d14b0695e8348e61b3
Dan Knorr <danieljknorr@gmail.com>
2018-04-23 19:31:14 -0400

    testing

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,10 +3,10 @@ let attempt = document.getElementById('attempt');
 
 function setHiddenFields() {
     answer = Math.floor(Math.random() * 10000);
-    while(answer.length < 4) {
-        answer = "0" + answer;
-    }
-    attempt = 0;
+//    while(answer.length < 4) {
+//        answer = "0" + answer;
+//    }
+//    attempt = 0;
 }
 
 //function guess() {
