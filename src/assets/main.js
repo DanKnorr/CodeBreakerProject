24321d09e73577e4588c551de96b8f2a44f3a374
Dan Knorr <danieljknorr@gmail.com>
2018-04-23 19:48:22 -0400

    after the vid

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,7 +2,7 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function setHiddenFields() {
-    answer = Math.floor(Math.random() * 10000);
+    answer.value = Math.floor(Math.random() * 10000).toString();
 //    while(answer.length < 4) {
 //        answer = "0" + answer;
 //    }
