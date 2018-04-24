2b2144a8173627a854c7bb71e331cac68358bc19
Dan Knorr <danieljknorr@gmail.com>
2018-04-23 20:18:44 -0400

    conversation avoidance

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,16 +3,16 @@ let attempt = document.getElementById('attempt');
 
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 10000).toString();
-//    while(answer.length < 4) {
-//        answer = "0" + answer;
-//    }
-//    attempt = 0;
+    while(answer.value.length < 4) {
+        answer.value = "0" + answer.value;
+    }
+    attempt = 0;
 }
 
-//function guess() {
-//    let input = document.getElementById('user-guess');
-//    if(answer === "" || attempt === "") {
-//        setHiddenFields();
-//    }
-//}
+function guess() {
+    let input = document.getElementById('user-guess');
+    if(answer.value == "" || attempt == "") {
+        setHiddenFields();
+    }
+}
 
